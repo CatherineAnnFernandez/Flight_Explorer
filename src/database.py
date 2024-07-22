cb7e46e83227e341d28c5bdd8a3a685981615d60
CatherineAnnFernandez <catherine.ann@pearldatadirect.com>
2024-07-22 18:39:09 +0400

    Add files via upload

---
+++ src/database.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+def connect_to_mysql():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="cathycerine101",
+            database="tickets"
+        )
+        if connection.is_connected():
+            print()
+            print("Welcome to Flight Explorer!")
+            print()
+            print('▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝')
+            print()
+            return connection
+    except Exception as e:
+        print("Error", e)
+        return None
